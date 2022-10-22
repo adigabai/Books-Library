49a04c95295cebbc5b03b8bb956106793ee4279f
Amit Hauzer <amit.hauzer@gmail.com>
2022-10-23 01:20:03 +0300

    loans: Bugs have been fixed

--- App/Data/data.py
+++ App/Data/data.py
@@ -12,25 +12,29 @@ def connect_to_db():
     cur = con.cursor()
     return con,cur
 
+
+
 # Books
 def get_all_books_from_db():
     # Connect
     con ,cur = connect_to_db()
-    # get data
+    # get all the books
     result = cur.execute('SELECT * from Books').fetchall()    
     # close
     con.close()
     return result
 
 
+
 def get_a_book_from_db(id_pk):
     # Connect
     con ,cur = connect_to_db()
-    # get data
+    # get a book 
     result = cur.execute(f'SELECT * from Books WHERE id={id_pk}').fetchone()
     # close
     con.close()
     return result
+
 
 
 def add_book_to_data(book, price, picture):
@@ -41,6 +45,7 @@ def add_book_to_data(book, price, picture):
     con.commit()
     # close
     con.close()
+
 
 
 def search_by_book_name(book_name):
@@ -54,8 +59,9 @@ def search_by_book_name(book_name):
     return results
 
 
+
 def del_book(id_pk):
-    print("book DELETED")
+    print("book has been deleted from db - Done.")
     # Connect
     con ,cur = connect_to_db()
     # delete a book
@@ -63,6 +69,7 @@ def del_book(id_pk):
     con.commit()
     # close
     con.close()
+
 
 
 # Users
@@ -76,17 +83,20 @@ def get_user(username):
     return result
 
 
+
 def add_user_to_data(username, eml, pet_name, password, permissions= 'customer'):
     # Connect
     con ,cur = connect_to_db()
     # hash password
     pass_hash = generate_password_hash(password)
-    # add book
+    # add user
     cur.execute(
         f"INSERT INTO Users ('username', 'email', 'pet_name', 'password', 'permissions') VALUES ('{username}', '{eml}', '{pet_name}', '{pass_hash}', '{permissions}')")
     con.commit()
     # close
     con.close()
+
+
 
 def user_loans(user_id):
     # Connect
@@ -96,6 +106,8 @@ def user_loans(user_id):
     # close
     con.close()
     return loans
+
+
 
 # Loans
 def add_loan_in_db(loan_date, return_date, book_id, user_id, returned=False):
@@ -107,11 +119,24 @@ def add_loan_in_db(loan_date, return_date, book_id, user_id, returned=False):
     # close
     con.close()
 
+
+
 def return_loan(book_id, user_id, return_date, returned=True):
     # Connect
     con ,cur = connect_to_db()
-    # add loan
+    # return loan
     cur.execute(f"UPDATE Loans SET return_date='{return_date}', returned='{returned}' WHERE book_id='{book_id}' AND user_id='{user_id}'")
+    con.commit()
+    # close
+    con.close()
+
+
+
+def return_loans_of_deleted_book(book_id, return_date, returned=True):
+    # Connect
+    con ,cur = connect_to_db()
+    # return all the loans of this book.
+    cur.execute(f"UPDATE Loans SET return_date='{return_date}', returned='{returned}' WHERE book_id='{book_id}' AND returned='False'")
     con.commit()
     # close
     con.close()
--- App/admin.py
+++ App/admin.py
@@ -1,7 +1,8 @@
+from datetime import date
 from flask import flash, render_template, request, redirect, Blueprint, g, url_for
 from App.login import login_required
-from App.upload_file import upload_file
-from App.Data.data import add_book_to_data, del_book, get_all_books_from_db
+from App.upload_file import delete_file, upload_file
+from App.Data.data import add_book_to_data, del_book, get_a_book_from_db, get_all_books_from_db, return_loans_of_deleted_book
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -13,9 +14,9 @@ def home_admin():
     if check_if_admin():
         books = get_all_books_from_db()
         return render_template('home_admin.html', data= books)
-
     else:
         return redirect(url_for('login.login'))
+
 
 
 # add a book
@@ -31,12 +32,10 @@ def add_book():
             print(f"{book},{price},{picture}")
             add_book_to_data(book=book, price=price, picture=picture.filename)
             return redirect('/admin/home')
-
         return render_template("add_book_form.html")
-
-
     else:
         return redirect(url_for('login.login'))
+
 
 
 # delete book
@@ -45,12 +44,17 @@ def add_book():
 def delete_book_from_db():
     if check_if_admin():
         id_pk = request.args.get('id')
+        book = get_a_book_from_db(id_pk = id_pk)
+        # delete the book's file.
+        delete_file(book_picture=book['Picture'])
+        # returns all the loans of this book.
+        return_loans_of_deleted_book(book_id= book['id'], return_date= date.today(), returned=True)
+        # delete the book from db.
         del_book(id_pk= id_pk)
         return redirect('/admin/home')
-
-
     else:
         return redirect(url_for('login.login'))
+
 
 
 def check_if_admin():
